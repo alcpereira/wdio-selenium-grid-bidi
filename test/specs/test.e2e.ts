27102eb586ca242c6725ae72f35c07346408dd39
Alexandre Pereira <alexandre.carneiro.pereira@gmail.com>
2025-05-15 16:53:29 +0100

    test: reproduction example for bidi screenshot

--- test/specs/test.e2e.ts
+++ test/specs/test.e2e.ts
@@ -1,16 +1,38 @@
-import { expect, browser, $ } from '@wdio/globals'
+import { browser } from "@wdio/globals";
+import { promises as fs } from "node:fs";
 
-describe('My Login application', () => {
-    it('should login with valid credentials', async () => {
-        await browser.url(`https://the-internet.herokuapp.com/login`)
-
-        await $('#username').setValue('tomsmith')
-        await $('#password').setValue('SuperSecretPassword!')
-        await $('button[type="submit"]').click()
-
-        await expect($('#flash')).toBeExisting()
-        await expect($('#flash')).toHaveText(
-            expect.stringContaining('You logged into a secure area!'))
-    })
-})
-
+describe("My Example", () => {
+  it("should take a nice screenshot", async () => {
+    const isBidi = !!browser.capabilities.webSocketUrl;
+    const screenshotName = isBidi
+      ? "screenshot_bidi.png"
+      : "screenshot_legacy.png";
+    await browser.url("https://fonts.google.com/specimen/Exile");
+    await browser.saveScreenshot(screenshotName);
+    if (isBidi) {
+      const context = await browser.getWindowHandle();
+      const screenshotDocument = await browser.browsingContextCaptureScreenshot(
+        {
+          context,
+          origin: "document",
+        },
+      );
+      await fs.writeFile(
+        "screenshot_bidi_raw_doc.png",
+        screenshotDocument.data,
+        "base64",
+      );
+      const screenshotViewport = await browser.browsingContextCaptureScreenshot(
+        {
+          context,
+          origin: "viewport",
+        },
+      );
+      await fs.writeFile(
+        "screenshot_bidi_raw_vp.png",
+        screenshotViewport.data,
+        "base64",
+      );
+    }
+  });
+});
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,23 +1,51 @@
 export const config: WebdriverIO.Config = {
-    runner: 'local',
-    tsConfigPath: './tsconfig.json',
-    specs: ['./test/specs/**/*.ts'],
-    exclude: [],
-    maxInstances: 10,
-    capabilities: [{
-        browserName: 'chrome',
-        "goog:chromeOptions": {
-            args: ['--headless']
-        }
-    }],
-    logLevel: 'error',
-    bail: 0,
-    waitforTimeout: 10000,
-    connectionRetryTimeout: 120000,
-    connectionRetryCount: 3,
-    framework: 'mocha',
-    reporters: ['spec'],
-    hostname: 'localhost',
-    port: 4444,
-    path: '/wd/hub',
-}
+  runner: "local",
+  tsConfigPath: "./tsconfig.json",
+  specs: ["./test/specs/**/*.ts"],
+  exclude: [],
+  maxInstances: 10,
+  capabilities: [
+    {
+      browserName: "chrome",
+      "wdio:enforceWebDriverClassic": true,
+      "goog:chromeOptions": {
+        args: ["disable-infobars"],
+        mobileEmulation: {
+          deviceMetrics: {
+            width: 375,
+            height: 812,
+            pixelRatio: 3,
+          },
+          userAgent:
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/17.4 Mobile/15A372 Safari/604.1",
+        },
+      },
+    },
+    {
+      browserName: "chrome",
+      "wdio:enforceWebDriverClassic": false,
+      "goog:chromeOptions": {
+        args: ["disable-infobars"],
+        mobileEmulation: {
+          deviceMetrics: {
+            width: 375,
+            height: 812,
+            pixelRatio: 3,
+          },
+          userAgent:
+            "Mozilla/5.0 (iPhone; CPU iPhone OS 11_0 like Mac OS X) AppleWebKit/604.1.38 (KHTML, like Gecko) Version/17.4 Mobile/15A372 Safari/604.1",
+        },
+      },
+    },
+  ],
+  logLevel: "debug",
+  bail: 0,
+  connectionRetryTimeout: 20_000,
+  connectionRetryCount: 0,
+  framework: "mocha",
+  // Uncomment if you want to use Docker selenium
+  // hostname: "localhost",
+  // waitforTimeout: 30_000,
+  // port: 4444,
+  // path: "/wd/hub",
+};
